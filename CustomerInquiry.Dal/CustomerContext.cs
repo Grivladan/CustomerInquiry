eb485f8ff452dd0f67b11bf06f71c59f168a758b
vladyslav <vladyslav@zwapgrid.com>
2019-08-27 13:48:56 +0000

    add connection, base and customer repository

---
+++ CustomerInquiry.Dal/CustomerContext.cs
@@ -0,0 +1,15 @@
+﻿using CustomerInquiry.Dal.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CustomerInquiry.Dal
+{
+    public class CustomerContext : DbContext
+    {
+        public CustomerContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ CustomerInquiry.Dal/RepositoryBase.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace CustomerInquiry.Dal
+{
+    public abstract class RepositoryBase<T> : IRepository<T> where T : class
+    {
+        protected CustomerContext CustomerContext { get; set; }
+
+        public RepositoryBase(CustomerContext customerContext)
+        {
+            this.CustomerContext = customerContext;
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return this.CustomerContext.Set<T>().AsNoTracking();
+        }
+
+        public IQueryable<T> GetByCondition(Expression<Func<T, bool>> expression)
+        {
+            return this.CustomerContext.Set<T>().Where(expression).AsNoTracking();
+        }
+
+        public void Create(T entity)
+        {
+            this.CustomerContext.Set<T>().Add(entity);
+        }
+
+        public void Update(T entity)
+        {
+            this.CustomerContext.Set<T>().Update(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            this.CustomerContext.Set<T>().Remove(entity);
+        }
+    }
+}
