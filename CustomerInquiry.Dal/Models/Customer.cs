060a834fa0a3e3a45ff9701a0aa12996d9a8ee17
vladyslav <vladyslav@zwapgrid.com>
2019-08-27 13:42:10 +0000

    add model classes

---
+++ CustomerInquiry.Dal/Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace CustomerInquiry.Dal.Models
+{
+    public class Customer
+    {
+        public Customer()
+        {
+            Transactions = new List<Transaction>();
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int PhoneNo { get; set; }
+        public List<Transaction> Transactions { get; set; }
+    }
+}
---
+++ CustomerInquiry.Dal/Models/Transaction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CustomerInquiry.Dal.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Amount { get; set; }
+        public string CurrencyCode { get; set; }
+        public Status Staus { get; set; }
+    }
+
+    public enum Status
+    {
+        Success,
+        Failed,
+        Canceled
+    }
+}
