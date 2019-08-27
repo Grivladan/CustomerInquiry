8a8e21b667bb9703add86dd9313ec71c2e8f5794
vladyslav <vladyslav@zwapgrid.com>
2019-08-27 14:11:39 +0000

    return single customer by inquiry

--- CustomerInquiry.Logic/Interfaces/ICustomerService.cs
+++ CustomerInquiry.Logic/Interfaces/ICustomerService.cs
@@ -1,10 +1,9 @@
 ﻿using CustomerInquiry.Dal.Models;
-using System.Collections.Generic;
 
 namespace CustomerInquiry.Logic.Interfaces
 {
     public interface ICustomerService
     {
-        ICollection<Customer> GetCustomersInfo(int? customerId = null, string email = null);
+        Customer GetCustomersInfo(int? customerId = null, string email = null);
     }
 }
--- CustomerInquiry.Logic/Services/CustomerService.cs
+++ CustomerInquiry.Logic/Services/CustomerService.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using CustomerInquiry.Dal;
 using CustomerInquiry.Dal.Models;
 using CustomerInquiry.Logic.Helpers;
@@ -15,10 +14,10 @@ namespace CustomerInquiry.Logic.Services
             _customerRepository = customerRepository;
         }
 
-        public ICollection<Customer> GetCustomersInfo(int? customerId = null, string email = null)
+        public Customer GetCustomerInfo(int? customerId = null, string email = null)
         {
             return _customerRepository.GetAll().WhereIf(customerId != null, x => x.Id == customerId)
-                .WhereIf(email != null, x => x.Email == email).ToList();
+                .WhereIf(email != null, x => x.Email == email).SingleOrDefault();
         }
     }
 }
